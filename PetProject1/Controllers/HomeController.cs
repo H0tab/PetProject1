1443645b9809ee0b1de101b6ee86effa33bfa6b0
Александр Князев <ksasha18@mail.ru>
2020-10-18 22:49:31 +0300

    Добавлена система регистрации

--- PetProject1/Controllers/HomeController.cs
+++ PetProject1/Controllers/HomeController.cs
@@ -11,27 +11,22 @@ namespace PetProject1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public IActionResult Registration()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Registration(User user)
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (ModelState.IsValid)
+            {
+                return Content("Регистрация прошла успешно!");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
---
+++ PetProject1/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetProject1.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Не указано имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не указан возраст")]
+        [Range(18, 120, ErrorMessage = "Недопустимый возраст")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Не указана почта")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректная почта")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Не указан адрес")]
+        public string Adress { get; set; }
+
+        [Required(ErrorMessage = "Не указан почтовый индекс")]
+        public int PostIndex { get; set; }
+    }
+}
